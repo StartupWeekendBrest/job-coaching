27fa5ea7c1c43dad93df780bef405951b364f73e
Joseph <josephcaillet@gmail.com>
2017-01-28 23:24:27 +0100

    created secteur activitees et géographiques

--- src/pages/secteur/secteur.ts
+++ src/pages/secteur/secteur.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { PseudoPage } from '../pseudo/pseudo';
 
 /*
   Generated class for the Secteur page.
@@ -12,11 +13,31 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'secteur.html'
 })
 export class SecteurPage {
+	items: Array<{region:string}> = [];
+	title: string = "Secteur(s) géographique(s)"
+	step: number = 0
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	  this.items.push({region: "Bretagne"})
+  }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SecteurPage');
   }
 
+	addLocationField() {
+		this.items.push({region: ""})
+	}
+
+	displayActivitiesSector() {
+		if(this.step == 0) {
+			this.title = "Secteur(s) d'activitée(s)"
+			this.items.length = 0
+			this.items.push({region: "Communication"})
+			this.step = 1
+		}
+		else {
+			this.navCtrl.push(PseudoPage)
+		}
+	}
 }
