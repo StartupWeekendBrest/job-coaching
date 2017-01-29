d37e3bc49db83ea618440ee6eedf4e58cbd6f3d0
Joseph <josephcaillet@gmail.com>
2017-01-29 09:49:05 +0100

    merge and main entry point set

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { TaskPage } from '../pages/task/task';
+import { LoginPage } from '../pages/login/login';
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = TaskPage;
+  rootPage = LoginPage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
